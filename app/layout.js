719b2e012fe12fdb803652b91eb53a2d08797298
div-vik <shivamambast5@gmail.com>
2024-02-15 23:20:08 +0530

    setup

---
+++ app/layout.js
@@ -0,0 +1,20 @@
+import "./globals.css";
+import Footer from "./ui/Footer";
+import Navbar from "./ui/Navbar";
+
+export const metadata = {
+  title: "Auto Verdure",
+  description: "An E-commerce application for plants and pots",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
