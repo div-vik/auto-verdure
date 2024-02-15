f51d62d2d71c10d88f6ff13862502e446ce348f2
div-vik <shivamambast5@gmail.com>
2024-02-15 23:29:43 +0530

    Add color value

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,16 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primaryMain: "#9A5CF5",
+        secondaryMain: "#809D79",
+        tertiaryMain: "#F9ECDD",
+        successAlerts: "#00C566",
+        errorAlerts: "#FF4747",
+        warningAlerts: "#FACC15",
+        primaryGrayscale: "#3D3D3D",
+        secondaryGrayscale: "#6F6E73",
+      },
     },
   },
   plugins: [],
