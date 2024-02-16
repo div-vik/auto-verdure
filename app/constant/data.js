fe035a14b5d689601009970872c9b951ec6e28bc
div-vik <shivamambast5@gmail.com>
2024-02-16 12:52:51 +0530

    Add required data

---
+++ app/constant/data.js
@@ -0,0 +1,25 @@
+export const navItems = [
+  { title: "Home", url: "/" },
+  { title: "About us", url: "/about-us" },
+  { title: "Store", url: "/store" },
+  { title: "Blog", url: "/blog" },
+  { title: "Contact", url: "/contact" },
+];
+
+export const quickLinks = [
+  { title: "Home", url: "/" },
+  { title: "Shop", url: "/shop" },
+  { title: "Service", url: "/service" },
+  { title: "Blog", url: "/blog" },
+  { title: "Disclaimer", url: "/disclaimer" },
+  { title: "My Cart", url: "/my-cart" },
+];
+
+export const support = [
+  { title: "About Us", url: "/about-us" },
+  { title: "Contact Us", url: "/contact-us" },
+  { title: "FAQ", url: "/faq" },
+  { title: "Terms & Conditions", url: "/terms-&-conditions" },
+  { title: "Shipping Policy", url: "/shipping-policy" },
+  { title: "Privacy Policy", url: "/privacy-policy" },
+];
