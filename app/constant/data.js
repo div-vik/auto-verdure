39976078377e7606b358cbfb0eb63af3526426d7
div-vik <shivamambast5@gmail.com>
2024-02-19 16:51:27 +0530

    Update data

--- app/constant/data.js
+++ app/constant/data.js
@@ -2,8 +2,16 @@ export const navItems = [
   { title: "Home", url: "/" },
   { title: "About us", url: "/about-us" },
   { title: "Store", url: "/store" },
-  { title: "Blog", url: "/blog" },
+  { title: "Blog", url: "/guides" },
   { title: "Contact", url: "/contact" },
+];
+
+export const profileNavItems = [
+  { title: "profile", url: "/profile" },
+  { title: "orders", url: "/profile/orders" },
+  { title: "guides", url: "/profile/guides" },
+  { title: "settings", url: "/profile/settings" },
+  { title: "payments", url: "/profile/payments" },
 ];
 
 export const quickLinks = [
@@ -22,4 +30,107 @@ export const support = [
   { title: "Terms & Conditions", url: "/terms-&-conditions" },
   { title: "Shipping Policy", url: "/shipping-policy" },
   { title: "Privacy Policy", url: "/privacy-policy" },
+];
+
+export const guides = [
+  {
+    blogImg: "/blogImage.png",
+    title: "The Benefits of Indoor Plants for Your Health and Well-Being",
+    authorName: "Prince",
+    authorImage: "/user.svg",
+    createdAt: "Feb 25, 2024",
+  },
+  {
+    blogImg: "/blogImage2.png",
+    title: "The Importance of Soil Quality for Healthy Plant Growth",
+    authorName: "Prince",
+    authorImage: "/user.svg",
+    createdAt: "Feb 25, 2024",
+  },
+  {
+    blogImg: "/blogImage3.png",
+    title: "The Best Plants for Hanging Baskets: A Guide to Elevated Greenery",
+    authorName: "Prince",
+    authorImage: "/user.svg",
+    createdAt: "Feb 25, 2024",
+  },
+  {
+    blogImg: "/blogImage4.png",
+    title:
+      "How to Incorporate Plants into Your Home Office for a Productivity Boost",
+    authorName: "Prince",
+    authorImage: "/user.svg",
+    createdAt: "Feb 25, 2024",
+  },
+  {
+    blogImg: "/blogImage5.png",
+    title: "The Importance of Soil Quality for Healthy Plant Growth",
+    authorName: "Prince",
+    authorImage: "/user.svg",
+    createdAt: "Feb 25, 2024",
+  },
+  {
+    blogImg: "/blogImage6.png",
+    title: "The Best Plants for Hanging Baskets: A Guide to Elevated Greenery",
+    authorName: "Prince",
+    authorImage: "/user.svg",
+    createdAt: "Feb 25, 2024",
+  },
+  {
+    blogImg: "/blogImage7.png",
+    title:
+      "How to Incorporate Plants into Your Home Office for a Productivity Boost",
+    authorName: "Prince",
+    authorImage: "/user.svg",
+    createdAt: "Feb 25, 2024",
+  },
+  {
+    blogImg: "/blogImage8.png",
+    title: "The Importance of Soil Quality for Healthy Plant Growth",
+    authorName: "Prince",
+    authorImage: "/user.svg",
+    createdAt: "Feb 25, 2024",
+  },
+  {
+    blogImg: "/blogImage9.png",
+    title: "The Best Plants for Hanging Baskets: A Guide to Elevated Greenery",
+    authorName: "Prince",
+    authorImage: "/user.svg",
+    createdAt: "Feb 25, 2024",
+  },
+  {
+    blogImg: "/blogImage10.png",
+    title:
+      "How to Incorporate Plants into Your Home Office for a Productivity Boost",
+    authorName: "Prince",
+    authorImage: "/user.svg",
+    createdAt: "Feb 25, 2024",
+  },
+];
+
+export const orders = [
+  {
+    productName: "Grobox with flower one flower two.",
+    location: "Mumbai,IN",
+    productType: "Product",
+    paymentStatus: "Paid",
+    deliveryStatus: "Arriving in 2 days",
+    lastUpdate: "Just now",
+  },
+  {
+    productName: "Grobox with flower one flower two.",
+    location: "Mumbai,IN",
+    productType: "Product",
+    paymentStatus: "Paid",
+    deliveryStatus: "Delivered",
+    lastUpdate: "12 hrs before",
+  },
+  {
+    productName: "Grobox with flower one flower two.",
+    location: "Mumbai,IN",
+    productType: "Flower",
+    paymentStatus: "Paid",
+    deliveryStatus: "Delivered",
+    lastUpdate: "12 hrs before",
+  },
 ];
